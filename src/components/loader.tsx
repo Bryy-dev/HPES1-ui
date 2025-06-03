5b311160163e557d2614937ea40227e9df7e34ea
Unchained <bryansurio@beesee.ph>
2025-06-03 12:22:37 +0800

    make size of loader  bigger

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
         <div className="flex justify-center py-12">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+            <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-blue-500"></div>
         </div>
     );
 };
