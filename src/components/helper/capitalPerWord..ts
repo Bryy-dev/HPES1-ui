979cae2de4a82e6039846d0dc38fa47e7cbcedd9
Unchained <bryansurio@beesee.ph>
2025-06-03 12:21:58 +0800

    added helper capitalize per word

---
+++ src/components/helper/capitalPerWord..ts
@@ -0,0 +1,5 @@
+export const capitalizeEachWord = (str: string): string =>
+    str
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
